618017acab842a481113cfcb55e4573f0d4e01be
czeskij <czeskij@gmail.com>
2023-12-04 23:05:37 +0100

    read maps dir

--- gameData/maps.go
+++ gameData/maps.go
@@ -1,6 +1,12 @@
 package gameData
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"go-gusanos/util"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 type MapConfig struct {
 	Spawnpoints [][]int64
@@ -10,4 +16,25 @@ type Map struct {
 	Level    *ebiten.Image
 	Material *ebiten.Image
 	Config   MapConfig
+}
+
+type Maps map[string]Map
+
+func LoadMaps(modName string) Maps {
+	log.Println("loading maps...")
+
+	maps := map[string]Map{}
+	directory := util.GetModDataPath(modName) + "/maps/"
+
+	// load files from mods/x/maps directory
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		panic("error: reading mod directory failed: " + err.Error())
+	}
+
+	for _, file := range files {
+		log.Println(file.Name(), file.IsDir())
+	}
+
+	return maps
 }
